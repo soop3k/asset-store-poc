71971d6431993d97670528b926f8d2e017f71522
soop3k <soop3k@gmail.com>
2025-09-24 18:28:33 +0200

    Let MapStruct handle link collection mapping

--- src/main/java/com/db/assetstore/infra/mapper/AssetLinkMapper.java
+++ src/main/java/com/db/assetstore/infra/mapper/AssetLinkMapper.java
@@ -4,10 +4,7 @@ import com.db.assetstore.domain.model.link.AssetLink;
 import com.db.assetstore.infra.jpa.link.AssetLinkEntity;
 import org.mapstruct.Mapper;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * MapStruct-backed mapper for converting asset link JPA entities into domain models.
@@ -17,15 +14,6 @@ public interface AssetLinkMapper {
 
     AssetLink toModel(AssetLinkEntity entity);
 
-    default List<AssetLink> toModels(Collection<AssetLinkEntity> entities) {
-        if (entities == null || entities.isEmpty()) {
-            return List.of();
-        }
-        return entities.stream()
-                .filter(Objects::nonNull)
-                .map(this::toModel)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
+    List<AssetLink> toModels(List<AssetLinkEntity> entities);
 }
 
